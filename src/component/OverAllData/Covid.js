9a003a9b7fb8127ef05d54086020310d75ae45c2
theakashsingh <singhaksh@gmail.com>
2021-05-27 13:59:56 +0530

    project completed

---
+++ src/component/OverAllData/Covid.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import './Covid.css';
+
+const Covid = () => {
+
+    const [data, setData] = useState([]);
+
+    const getCovidData = async () => {
+        try {
+            const res = await fetch("https://api.covid19india.org/data.json");
+            const actualData = await res.json();
+            console.log(actualData.statewise[0]);
+            setData(actualData.statewise[0]);
+        } catch (err) {
+           console.log(err); 
+        }
+       
+    }
+
+    useEffect(() => {
+         getCovidData();  
+    }, []);
+
+    return (
+        <>
+        
+          <h1>🔴 LIVE</h1> 
+          <h2>COVID-19 CORONAVIRUS TRACKER</h2> 
+            <div className="card">
+
+                <div className="card_main">
+                  <div className="card_inner">
+                          <p className="card_name"><span>OUR</span>COUNTRY</p>
+                          <p className="carda_total card_small">INDIA</p>
+                  </div>
+              
+              
+                  <div className="card_inner">
+                      
+                          <p className="card_name"><span>TOTAL</span>RECOVERED</p>
+                          <p className="carda_total card_small">{data.recovered}</p>
+                  </div>
+              
+            
+                  <div className="card_inner">
+                      
+                          <p className="card_name"><span>TOTAL</span>CONFIRMED</p>
+                          <p className="carda_total card_small">{data.confirmed}</p>
+                  </div>
+                </div>
+              
+                <div className="card_main">
+                  <div className="card_inner">
+                          <p className="card_name"><span>TOTAL</span>DEATH</p>
+                          <p className="carda_total card_small">{data.deaths}</p>
+                  </div>
+              
+              
+                  <div className="card_inner">
+                          <p className="card_name"><span>TOTAL</span>ACTIVE</p>
+                          <p className="carda_total card_small">{data.active}</p>
+                  </div>
+              
+              
+                  <div className="card_inner">
+                          <p className="card_name"><span>LAST</span>UPDATED</p>
+                          <p className="carda_total card_small">{data.lastupdatedtime}</p>
+
+                  </div>
+                </div>
+
+            </div>
+            
+           
+        </>
+    )
+}
+
+export default Covid
---
+++ src/component/StateWiseData/StateWise.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./StateWise.css";
+
+const StateWise = () => {
+    return (
+        <>
+          <h1>Hello</h1>  
+        </>
+    )
+}
+
+export default StateWise
+
+
